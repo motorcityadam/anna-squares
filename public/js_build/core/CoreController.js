92bf4362bdce1559ba1985ff7b561a7937f4ea88
Adam Joseph Cook <acook@alliedstrand.com>
2013-12-29 19:39:24 -0600

    Updated to version v0.1.2.

--- public/js_build/core/CoreController.js
+++ public/js_build/core/CoreController.js
@@ -1,2 +1,2 @@
-/*! anna-squares - v0.1.1 - 29-12-2013 */
+/*! anna-squares - v0.1.2 - 29-12-2013 */
 "use strict";annaSquaresApp.controller("coreController",function($scope){$scope.test="This is the core controller."});
