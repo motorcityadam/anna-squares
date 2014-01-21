64b25ccc881dec91937af6a875bfacfa28f9667f
Adam Joseph Cook <acook@alliedstrand.com>
2014-01-20 19:09:08 -0600

    fix(routingConfig.js): Removed too long line and double-quote lint.

--- client/src/common/routingConfig.js
+++ client/src/common/routingConfig.js
@@ -72,7 +72,8 @@
             bitMask: parseInt(resultBitMask, 2)
           };
         }
-        else console.log("Access Control Error: Could not parse '" + accessLevelDeclarations[level] + "' as access definition for level '" + level + "'")
+        else console.log('Access Control Error: Could not parse "' +
+            accessLevelDeclarations[level] + '" as access definition for level "' + level + '"');
 
       }
       else {
@@ -81,7 +82,8 @@
         for(role in accessLevelDeclarations[level]){
           if(userRoles.hasOwnProperty(accessLevelDeclarations[level][role]))
             resultBitMask = resultBitMask | userRoles[accessLevelDeclarations[level][role]].bitMask;
-          else console.log("Access Control Error: Could not find role '" + accessLevelDeclarations[level][role] + "' in registered roles while building access for '" + level + "'")
+          else console.log('Access Control Error: Could not find role "' +
+              accessLevelDeclarations[level][role] + '" in registered roles while building access for "' + level + '"');
         }
         accessLevels[level] = {
           bitMask: resultBitMask
